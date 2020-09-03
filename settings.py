5ba89dbb2c8804cb07a86ac19d7ce8098fdf4f50
Chris39 <christopher.burrows@griffithuni.edu.au>
2020-09-03 17:41:06 +1000

    Changed Player colour

--- settings.py
+++ settings.py
@@ -6,6 +6,9 @@ WIDTH, HEIGHT = 610, 670
 MAZE_WIDTH, MAZE_HEIGHT = WIDTH - TOP_BOTTOM_BUFFER, HEIGHT - TOP_BOTTOM_BUFFER
 FPS = 60
 
+ROWS = 30
+COLS = 28
+
 
 #colour settings
 BLACK = (0,0,0)
@@ -13,7 +16,7 @@ GREY = (107,107,107)
 RED = (208,22,22)
 WHITE = (255,255,255)
 ORANGE = (217, 155, 22)
-YELLOW = (222, 210, 42)
+PLAYER_COLOUR = (222, 210, 42)
 AQUA = (15, 153, 146)
 LILAC = (196, 167, 194)
 #font settings
@@ -21,5 +24,6 @@ START_FONT_SIZE = 18
 START_FONT = 'arial black'
 
 #player settings
-PLAYER_START_POS = vec(1,1)
+# PLAYER_START_POS = vec(1,1)
 #enemy settings
+
